6ab13fa1897dc2db111feb19f76e3b15b924d019
trias <triasnugra97@gmail.com>
2023-11-17 22:25:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+
+function updateCountdown() {
+  const currentDate = new Date();
+  const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  const endOfYear = new Date(currentDate.getFullYear() + 1, 0, 0);
+  
+  const daysLeftInMonth = Math.ceil((endOfMonth - currentDate) / (1000 * 60 * 60 * 24));
+  const daysLeftInYear = Math.ceil((endOfYear - currentDate) / (1000 * 60 * 60 * 24));
+  
+  document.getElementById('countdown-month').innerText = daysLeftInMonth + " days";
+  document.getElementById('countdown-year').innerText = daysLeftInYear + " days";
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
