37c9ec72ad40ba47a8c3b9340d2ae63c1182d473
trias <triasnugra97@gmail.com>
2023-11-17 22:36:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-function updateTime() {
-  const currentDate = new Date();
-
-  const dateElement = document.getElementById('date');
-  const timeElement = document.getElementById('time');
-
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const dateString = currentDate.toLocaleDateString('en-US', options);
-  const timeString = currentDate.toLocaleTimeString('en-US');
-
-  dateElement.textContent = dateString;
-  timeElement.textContent = timeString;
+// Function to display current date and time
+function showDateTime() {
+  const now = new Date();
+  const dateTimeElement = document.getElementById('date-time');
+  dateTimeElement.textContent = now.toLocaleString();
 }
 
-updateTime();
-setInterval(updateTime, 1000);
-
-
-function updateCountdown() {
-  const currentDate = new Date();
-  const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-  const endOfYear = new Date(currentDate.getFullYear() + 1, 0, 0);
-  
-  const daysLeftInMonth = Math.ceil((endOfMonth - currentDate) / (1000 * 60 * 60 * 24));
-  const daysLeftInYear = Math.ceil((endOfYear - currentDate) / (1000 * 60 * 60 * 24));
-  
-  document.getElementById('countdown-month').innerText = daysLeftInMonth + " days";
-  document.getElementById('countdown-year').innerText = daysLeftInYear + " days";
+// Function to calculate monthly countdown
+function calculateMonthlyCountdown() {
+  const now = new Date();
+  const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  const daysLeft = Math.ceil((endOfMonth - now) / (1000 * 60 * 60 * 24));
+  document.querySelector('.teal').textContent = `Days left this month: ${daysLeft}`;
 }
 
-updateCountdown();
-setInterval(updateCountdown, 1000);
+// Function to calculate yearly countdown
+function calculateYearlyCountdown() {
+  const now = new Date();
+  const endOfYear = new Date(now.getFullYear() + 1, 0, 0);
+  const daysLeft = Math.ceil((endOfYear - now) / (1000 * 60 * 60 * 24));
+  document.querySelector('.steelblue').textContent = `Days left this year: ${daysLeft}`;
+}
+
+// Array of motivational quotes
+const quotes = [
+  "Quote 1",
+  "Quote 2",
+  "Quote 3",
+  // Add more quotes as needed
+];
+
+// Function to display random quote every 5 seconds
+function displayRandomQuote() {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  document.querySelector('.monospace').textContent = randomQuote;
+}
+
+// Initial function calls
+showDateTime();
+calculateMonthlyCountdown();
+calculateYearlyCountdown();
+displayRandomQuote();
+
+// Update time and quotes at intervals
+setInterval(showDateTime, 1000);
+setInterval(displayRandomQuote, 5000);
